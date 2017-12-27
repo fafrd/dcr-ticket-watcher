c5616b080391b929435549eaaec995bea8499eb9
Quintinity <vlad.marica1@gmail.com>
2017-12-26 23:48:17 -0400

    Update ticketd

--- ticketd/ticket.py
+++ ticketd/ticket.py
@@ -4,12 +4,16 @@ class TicketStatus(Enum):
     LIVE = 'Live'
     VOTED = 'Voted'
     MISSED = 'Missed'
+    IMMATURE = 'Immature'
+    MEMPOOL = 'Mempool'
 
 class Ticket():
-    def __init__(self, txhash, purchase_date, price, status):
+    def __init__(self, txhash, purchase_date, price, fee, status):
         self.txhash = txhash
         self.price = price
         self.status = status
         self.purchase_date = purchase_date
+        self.fee = fee
+        self.block = 0
         self.vote_date = None
 
--- ticketd/ticketd.py
+++ ticketd/ticketd.py
@@ -3,13 +3,17 @@ import subprocess
 import signal
 import sys
 import json
-from datetime import datetime
 
+from datetime import datetime
 from ticket import Ticket, TicketStatus
 
 dcrwallet_log_file = os.path.expanduser('~/.dcrwallet/logs/mainnet/dcrwallet.log')
 datetime_format = '%Y-%m-%d %H:%M:%S'
+
 tickets = {}
+mempool_tickets = set()
+immature_tickets = set()
+block_height = 0
 
 def signal_handler(signal, frame):
     print('\nExiting ticketd...')
@@ -19,13 +23,32 @@ def execute_dcrwallet_command(command):
     result = subprocess.check_output(['dcrctl', '--wallet'] + command).decode('utf-8')
     return json.loads(result)
 
+def handle_new_block():
+    mempool_tickets_copy = [x for x in mempool_tickets]
+    for ticket in mempool_tickets_copy:
+        txinfo = execute_dcrwallet_command(['gettransaction', ticket.txhash])
+        if txinfo.blockhash != '':
+            ticket.status = TicketStatus.IMMATURE
+            immature_tickets.add(ticket)
+            mempool_tickets.remove(ticket)
+    
+    immature_tickets_copy = [x for x in immature_tickets]
+    for ticket in immature_tickets_copy:
+        if block_height - ticket.block > 256:
+            ticket.status = TicketStatus.LIVE
+            immature_tickets.remove(ticket)
+
 def handle_new_ticket(line):
     pieces = line.split()
     purchase_date = datetime.strptime(' '.join(pieces[:2])[:-4], datetime_format)
     txhash = pieces[-1]
-    price = 0 #TODO
-    ticket = Ticket(txhash, purchase_date, price, TicketStatus.LIVE)
     
+    txinfo = execute_dcrwallet_command(['gettransaction', txhash])
+    price = abs(txinfo['details'][0]['amount'])
+    fee = txinfo['details'][0]['fee']
+
+    ticket = Ticket(txhash, purchase_date, price, fee, TicketStatus.MEMPOOL)
+    mempool_tickets.add(ticket)
     tickets[txhash] = ticket
 
 def handle_vote(line):
@@ -51,9 +74,9 @@ def handle_miss(line):
 
 def print_tickets():
     os.system('clear')
-    print('status\ttxhash')
+    print('status\ttxhash\tprice')
     for ticket in tickets.values():
-        print(ticket.status.name + '\t' + ticket.txhash)
+        print(ticket.status.name + '\t' + ticket.txhash + '\t' + str(ticket.price))
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
@@ -64,7 +87,7 @@ def main():
     f = subprocess.Popen(['tail', '-n', '+1', '-f', dcrwallet_log_file], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     while True:
         line = f.stdout.readline().decode('utf-8');
-        
+
         if 'Successfully sent SStx purchase transaction' in line:
             handle_new_ticket(line)
             print_tickets()
@@ -73,7 +96,9 @@ def main():
             print_tickets()
         elif 'Failed to sign vote for ticket hash' in line:
             handle_miss(line)
-            print_tickets() 
+            print_tickets()
+        elif 'Connecting block' in line:
+            block_height = int(line.split()[-1])
+            handle_new_block()
     
-
 main()
